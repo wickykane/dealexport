abf744cb7b958effa5ad6c5f50bd7dd88fdf4de3
huy.dao2 <huy.dao@hubble.sg>
2020-09-23 18:14:15 +0700

    add price product

--- wp-content/themes/dealexport/woocommerce/single-product/price.php
+++ wp-content/themes/dealexport/woocommerce/single-product/price.php
@@ -23,3 +23,17 @@ global $product;
     <meta itemprop="priceCurrency" content="<?php echo get_woocommerce_currency(); ?>" />
     <link itemprop="availability" href="http://schema.org/<?php echo $product->is_in_stock() ? 'InStock' : 'OutOfStock'; ?>" />
 </div>
+<div class="item-price" style="clear: both;" itemprop="offers" itemscope itemtype="http://schema.org/Offer">
+    <span><?php
+    _e('DepartingAvec', 'dealexport');?> :</span>
+    <?php echo $product->get_price_html();
+    ?>
+    <?php // $arv_price = get_post_meta($product->id, 'arv_price', true); ?><br>
+    <!-- <span><?php
+    // _e('Arrival', 'dealexport');?>:</span> -->
+    <?php // echo woocommerce_price($arv_price);
+    ?>
+    <meta itemprop="price"  content="<?php echo $product->get_price(); ?>" />
+    <meta itemprop="priceCurrency" content="<?php echo get_woocommerce_currency(); ?>" />
+    <link itemprop="availability" href="http://schema.org/<?php echo $product->is_in_stock() ? 'InStock' : 'OutOfStock'; ?>" />
+</div>
