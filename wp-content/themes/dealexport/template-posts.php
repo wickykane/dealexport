6a857354fb97be0f6f09f911885c6ae549544866
huy.dao2 <huy.dao@hubble.sg>
2020-09-25 00:17:29 +0700

    add blog

--- wp-content/themes/dealexport/template-posts.php
+++ wp-content/themes/dealexport/template-posts.php
@@ -5,44 +5,44 @@ Template Name: Posts
 
 get_header();
 
-$layout=ThemedbCore::getOption('posts_layout', 'right');
-if($layout=='left') {
+$layout = ThemedbCore::getOption('posts_layout', 'right');
+if ($layout == 'left') {
 ?>
-<aside class="sidebar column fourcol">
+  <aside class="sidebar column fourcol mt-3 mod-sidebar">
     <?php get_sidebar(); ?>
-</aside>
-<div class="column eightcol last">
-<?php } else if($layout=='right') { ?>
-<div class="column eightcol">
-<?php } else { ?>
-<div class="fullcol">
-<?php } ?>
-    <?php echo category_description(); ?>
-    <div class="posts-wrap">
+  </aside>
+  <div class="column eightcol last">
+  <?php } else if ($layout == 'right') { ?>
+    <div class="column eightcol  mt-3">
+    <?php } else { ?>
+      <div class="fullcol">
+      <?php } ?>
+      <?php echo category_description(); ?>
+      <div class="posts-wrap et_pb_blog_grid ">
         <?php
-        if(is_page()) {
-            query_posts(array(
-                'post_type' =>'post',
-                'paged' => themedb_paged(),
-            ));
+        if (is_page()) {
+          query_posts(array(
+            'post_type' => 'post',
+            'paged' => themedb_paged(),
+          ));
         }
-        
-        if(have_posts()) {
-            while(have_posts()) {
-                the_post(); 
-                get_template_part('content', 'post');
-            } 
+
+        if (have_posts()) {
+          while (have_posts()) {
+            the_post();
+            get_template_part('content', 'post');
+          }
         } else {
         ?>
-        <h3><?php _e('No posts found. Try a different search?','dealexport'); ?></h3>
-        <p><?php _e('Sorry, no posts matched your search. Try again with some different keywords.','dealexport'); ?></p>
-        <?php } ?>		
-    </div>
-    <?php ThemedbInterface::renderPagination(); ?>
-</div>
-<?php if($layout=='right') { ?>
-<aside class="sidebar column fourcol last">
-    <?php get_sidebar(); ?>
-</aside>
-<?php } ?>
-<?php get_footer(); ?>
+          <h3><?php _e('No posts found. Try a different search?', 'dealexport'); ?></h3>
+          <p><?php _e('Sorry, no posts matched your search. Try again with some different keywords.', 'dealexport'); ?></p>
+        <?php } ?>
+      </div>
+      <?php ThemedbInterface::renderPagination(); ?>
+      </div>
+      <?php if ($layout == 'right') { ?>
+        <aside class="mt-3 sidebar column fourcol last  mod-sidebar">
+          <?php get_sidebar(); ?>
+        </aside>
+      <?php } ?>
+      <?php get_footer(); ?>
