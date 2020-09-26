578a10d558fb0feb83c63348c3ca885206188c92
huy.dao2 <huy.dao@hubble.sg>
2020-09-26 07:34:57 +0700

    change config

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'local');
+define('DB_NAME', 'dealexport');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'wordpress');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'root');
+define('DB_PASSWORD', '5xaD5YNttOj98FQXz9PXMWxAXudQxGcR7aP44SZFUA');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
